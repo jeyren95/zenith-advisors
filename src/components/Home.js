7806787923cc6297b7ca2f57a6a7679514ab6115
Poon Jey-ren <jeyren95@hotmail.com>
2021-03-08 23:15:54 +0800

    removed password

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,29 +6,15 @@ import LifeAtZenith from "./life-at-zenith/LifeAtZenith";
 import Contact from "./contact/Contact";
 import Footer from "./footer/Footer";
 
-import history from "../history"
 import "./Home.css";
 
 class Home extends React.Component {
-  authorizeUser() {
-    const password = prompt("Enter password")
-    if (password === "zenithalliance") {
-      history.push("/")
-    } else if (password !== "zenithalliance") {
-      window.location.reload(true);
-    }
-  }
-
-  componentDidMount() {
-    this.authorizeUser();
-  }
 
   render() {
     return (
       <div>
         <HomeIntroduction />
         <Leaders />
-
         <LifeAtZenith />
         <Contact />
         <Footer />
